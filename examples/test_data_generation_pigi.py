b63f82808e3a078b32542926d6e97b19b045e1e2
RAraghavarora <f20171016p@pilani.bits-pilani.ac.in>
2024-06-29 17:58:24 +0530

    Add function tracking to test_data_generation

--- examples/test_data_generation_pigi.py
+++ examples/test_data_generation_pigi.py
@@ -2,13 +2,15 @@ import config
 
 from cogarch_tools.cogarch_run import run_agent
 from cogarch_tools.processes.pddlstream_agent import PDDLStreamAgent
-
+import tracit
+import sys
+sys.setprofile(tracit.tracefunc)
 
 if __name__ == '__main__':
     seed = 378277
     goal_variations = [3]
     run_agent(
         agent_class=PDDLStreamAgent, config='config_pigi.yaml',
-        goal_variations=goal_variations, seed=seed
+        goal_variations=goal_variations, seed=seed, exp_subdir='piginet_data', problem='test_full_kitchen'
     )
-
+    #TODO: Why is problem and exp_subdir not being loaded by themselves? (Raghav)
